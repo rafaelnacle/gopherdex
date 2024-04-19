b30cfe1511cd5ee00666171788d370252733ee8f
Rafael Nacle <54647722+rafaelnacle@users.noreply.github.com>
2024-04-19 19:15:58 -0300

    added getCommands func to add dynamic commands instead of hardcoded

--- repl.go
+++ repl.go
@@ -15,9 +15,44 @@ func initializeRepl() {
 		fmt.Print(" >")
 		scanner.Scan()
 		text := scanner.Text()
-		cleaned := cleanInput(text)
 
-		fmt.Println("text: ", cleaned)
+		cleaned := cleanInput(text)
+		if len(cleaned) == 0 {
+			continue
+		}
+
+		commandName := cleaned[0]
+
+		availableCommands := getCommands()
+
+		command, ok := availableCommands[commandName]
+		if !ok {
+			fmt.Println("Invalid command")
+			continue
+		}
+
+		command.callback()
+	}
+}
+
+type cliCommand struct {
+	name        string
+	description string
+	callback    func()
+}
+
+func getCommands() map[string]cliCommand {
+	return map[string]cliCommand{
+		"help": {
+			name:        "help",
+			description: "Prints the help menu",
+			callback:    callbackHelp,
+		},
+		"exit": {
+			name:        "exit",
+			description: "Exit from Gopherdex",
+			callback:    callbackExit,
+		},
 	}
 }
 
