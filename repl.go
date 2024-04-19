a909a034f40a27d332c0bef70a1e42dbabc97bba
Rafael Nacle <54647722+rafaelnacle@users.noreply.github.com>
2024-04-19 19:53:49 -0300

    added map command to repl

--- repl.go
+++ repl.go
@@ -48,6 +48,11 @@ func getCommands() map[string]cliCommand {
 			description: "Prints the help menu",
 			callback:    callbackHelp,
 		},
+		"map": {
+			name:        "map",
+			description: "Lists some location areas",
+			callback:    callbackMap,
+		},
 		"exit": {
 			name:        "exit",
 			description: "Exit from Gopherdex",
