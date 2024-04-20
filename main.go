646d6e0db76d9a2400b273a9bc159952e6c8ac68
Rafael Nacle <54647722+rafaelnacle@users.noreply.github.com>
2024-04-19 21:25:05 -0300

    added config struct

--- main.go
+++ main.go
@@ -1,5 +1,18 @@
 package main
 
+import "github.com/rafaelnacle/gopherdex/internal/pokeapi"
+
+type config struct {
+	pokeapiClient       pokeapi.Client
+	nextLocationAreaURL *string
+	prevLocationAreaURL *string
+}
+
 func main() {
-	initializeRepl()
+
+	cfg := config{
+		pokeapiClient: pokeapi.NewClient(),
+	}
+
+	initializeRepl(&cfg)
 }
