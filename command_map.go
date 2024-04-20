001d97ddba210d7b992b6c74c0eecfe2ba19782e
Rafael Nacle <54647722+rafaelnacle@users.noreply.github.com>
2024-04-19 21:24:48 -0300

    added pagination on the location areas

--- command_map.go
+++ command_map.go
@@ -1,22 +1,40 @@
 package main
 
 import (
+	"errors"
 	"fmt"
-	"log"
-
-	"github.com/rafaelnacle/gopherdex/internal/pokeapi"
 )
 
-func callbackMap() error {
-	pokeapiClient := pokeapi.NewClient()
-	resp, err := pokeapiClient.ListLocationAreas()
+func callbackMap(cfg *config) error {
+	resp, err := cfg.pokeapiClient.ListLocationAreas(cfg.nextLocationAreaURL)
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
+
 	fmt.Println("Location areas: ")
 	for _, area := range resp.Results {
 		fmt.Printf(" - %s\n", area.Name)
 	}
 
+	cfg.nextLocationAreaURL = resp.Next
+	cfg.prevLocationAreaURL = resp.Previous
+	return nil
+}
+func callbackMapb(cfg *config) error {
+	if cfg.prevLocationAreaURL == nil {
+		return errors.New("you're on the first page")
+	}
+	resp, err := cfg.pokeapiClient.ListLocationAreas(cfg.prevLocationAreaURL)
+	if err != nil {
+		return err
+	}
+
+	fmt.Println("Location areas: ")
+	for _, area := range resp.Results {
+		fmt.Printf(" - %s\n", area.Name)
+	}
+
+	cfg.nextLocationAreaURL = resp.Next
+	cfg.prevLocationAreaURL = resp.Previous
 	return nil
 }
